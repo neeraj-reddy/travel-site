a12f00fcf560777724fc4d262d704bdd88ce4ca0
Neeraj <34216876+reddyneeraj@users.noreply.github.com>
2018-05-03 23:49:27 +0530

    Completed mobile menu

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,16 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import mobileMenu from './modules/mobile-menu';
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $0 in taxes.");
-    }
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+var mobileNav = new mobileMenu();
