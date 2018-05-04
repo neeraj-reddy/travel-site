8f6669694e1281897a7b1d57d88f529733b766ca
Neeraj <34216876+reddyneeraj@users.noreply.github.com>
2018-05-04 13:14:37 +0530

    Added sticky navigation functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import mobileMenu from './modules/mobile-menu';
 import revealOnScroll from './modules/reveal-on-scroll';
 import $ from "jquery";
+import stickyHeader from './modules/sticky-header';
 
 var mobileNav = new mobileMenu();
 new revealOnScroll($(".feature-item"), "85%");
 new revealOnScroll($(".testimonial"), "60%");
+var stickyNav = new stickyHeader();
