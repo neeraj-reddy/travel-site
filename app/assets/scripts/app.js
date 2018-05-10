6ac3b971c9bb224d96d51838a0397e425f75a0b0
Neeraj <34216876+reddyneeraj@users.noreply.github.com>
2018-05-10 18:51:34 +0530

    Created modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import mobileMenu from './modules/mobile-menu';
 import revealOnScroll from './modules/reveal-on-scroll';
 import $ from "jquery";
 import stickyHeader from './modules/sticky-header';
+import Modal from './modules/modal';
 
 var mobileNav = new mobileMenu();
 new revealOnScroll($(".feature-item"), "85%");
 new revealOnScroll($(".testimonial"), "60%");
 var stickyNav = new stickyHeader();
+var modal = new Modal();
